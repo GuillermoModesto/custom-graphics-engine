fb8de4db679f417b682c83dd64f89057bea56ddb
GuillermoModesto <guillermomodestoanti@gmail.com>
2025-08-09 21:33:04 +0200

    basic setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,11 @@
+#include <windows.h>
+#include <iostream>
+
+int main() {
+    std::cout << "BlueBrains started!" << std::endl;
+
+    MessageBoxW(nullptr, L"BlueBrains started!", L"BlueBrains", MB_OK);
+
+    return 0;
+}
 
